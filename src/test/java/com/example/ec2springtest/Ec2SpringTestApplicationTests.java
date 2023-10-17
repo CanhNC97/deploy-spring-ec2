4f79de484f14826d4f17b23614d36af6daaed790
Nguyen <canhcong.nguyen@dxc.com>
2023-10-17 16:43:34 +0700

    first commit

---
+++ src/test/java/com/example/ec2springtest/Ec2SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
